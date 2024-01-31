dfae533ee0fcb4aebc1a2c15eaca77dc3e88b65a
OKSUchun <oksuchun119@gmail.com>
2024-01-31 16:46:25 +0900

    chore: Add dummy data in member table

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ class Board(db.Model):
 
 class Board_Reply(db.Model):
     reply_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False) # NOT NULL
     created_dttm = db.Column(db.DateTime, default=datetime.utcnow)
     updated_dttm = db.Column(db.DateTime, default=datetime.utcnow)
     board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
-    board = db.relationship('Board', backref=db.backref('replies', lazy=True))
+    board = db.relationship('Board', backref=db.backref('replies', lazy=True)) # TODO: replies 양방향 참조 필요한 경우 확인
     user_id = db.Column(db.String, db.ForeignKey('member.user_id'), nullable=False)
-    member = db.relationship('Member')
+    member = db.relationship('Member') # TODO: relationship 에 대한 정리
 
     def __repr__(self):
         return f'BoardReply(content={self.content}, board_id={self.board_id}, user_id={self.user_id})'
