6e4729ab42b98b6f959903d581e01109f873c3cc
조성현 <cshcho99@gmail.com>
2024-01-31 23:37:31 +0900

    Fixing bugs regarding csrf

--- app.py
+++ app.py
@@ -13,53 +13,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))  # 지금 현재 위치
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 
-db = SQLAlchemy(app)
-
-
-class Member(db.Model):
-    user_id = db.Column(db.String(30), primary_key=True)
-    pwd = db.Column(db.String(150), nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f"Member(user_id={self.user_id}, name={self.name})"
-
-
-class Board(db.Model):
-    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(300), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    created_dttm = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_dttm = db.Column(db.DateTime, default=datetime.utcnow)
-    image_url = db.Column(db.String(300), nullable=True)
-    user_id = db.Column(db.String, db.ForeignKey("member.user_id"), nullable=False)
-    member = db.relationship("Member", backref=db.backref("boards", lazy=True))
-
-    def __repr__(self):
-        return (
-            f"Board(title={self.title}, content={self.content}, user_id={self.user_id})"
-        )
-
-
-class Board_Reply(db.Model):
-    reply_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.Text, nullable=False)  # NOT NULL
-    created_dttm = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_dttm = db.Column(db.DateTime, default=datetime.utcnow)
-    board_id = db.Column(db.Integer, db.ForeignKey("board.board_id"), nullable=False)
-    board = db.relationship(
-        "Board", backref=db.backref("replies", lazy=True)
-    )
-    user_id = db.Column(db.String, db.ForeignKey("member.user_id"), nullable=False)
-    member = db.relationship("Member")  # TODO: relationship 에 대한 정리
-
-    def __repr__(self):
-        return f"BoardReply(content={self.content}, board_id={self.board_id}, user_id={self.user_id})"
-
-
-with app.app_context():
-    db.create_all()
-
 
 @app.route("/")
 def home():
@@ -160,8 +113,7 @@ if __name__ == "__main__":
 #수정사항에 대한 track을 하지 않는다. True로 한다면 warning 메시지유발
     app.config['SECRET_KEY'] = 'wcsfeufhwiquehfdx'
     app.config['SESSION_PERMANENT'] = False
-    csrf = CSRFProtect()
-    csrf.init_app(app)
+
 
     db.init_app(app)
     with app.app_context():
